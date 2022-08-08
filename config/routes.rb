948955bae5a9eea70d52fc2cfb0b600c7767445e
arielberardi <ariel.berardi95@gmail.com>
2022-08-08 18:27:13 -0300

    Clean unused routes and improve devise routes names

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: %i[sessions registrations]
   as :user do
-    post 'users/sign_up', to: 'devise/registrations#create'
+    get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
+    post 'sign_in', to: 'devise/sessions#create', as: :user_session
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+
+    get 'sign_up', to: 'devise/registrations#new', as: :new_user_registration
+    post 'sign_up', to: 'devise/registrations#create', as: :user_registration
   end
 
   resources :groups do
-    resources :posts, except: %i[new edit]
+    resources :posts, except: %i[index new edit]
   end
 
-  resources :posts do
+  resources :posts, only: %i[] do
     resources :comments, only: %i[create update destroy]
   end
 
