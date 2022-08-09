d40eb85cb6268ed594aaaebd34afad39ebf595ad
arielberardi <ariel.berardi95@gmail.com>
2022-08-08 21:23:57 -0300

    Fix all rubocop issues

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,27 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_post
-  before_action :set_comment, only: %i[show edit update destroy]
+  before_action :set_comment, only: %i[edit update destroy]
 
   # POST /comments
   def create
     @comment = @post.comments.new(comment_params)
 
     if @comment.save
-      redirect_to redirect_to_post, notice: 'Comment was successfully created.'
+      redirect_to redirect_to_post, notice: locale('created')
     else
       redirect_to redirect_to_post, status: :unprocessable_entity
     end
   end
 
-  def edit
-  end
+  def edit; end
 
   # PATCH/PUT /comments/1
   def update
     if @comment.update(comment_params)
-      redirect_to redirect_to_post, notice: 'Comment was successfully updated.'
+      redirect_to redirect_to_post, notice: locale('updated')
     else
       redirect_to redirect_to_post, status: :unprocessable_entity
     end
@@ -31,7 +32,7 @@ class CommentsController < ApplicationController
     @comment.destroy
 
     # Using turbo requires to return status. https://github.com/rails/rails/issues/44170
-    redirect_to redirect_to_post, status: :see_other, notice: 'Comment was successfully destroyed.'
+    redirect_to redirect_to_post, status: :see_other, notice: locale('destroyed')
   end
 
   private
@@ -50,5 +51,9 @@ class CommentsController < ApplicationController
 
   def redirect_to_post
     group_post_url(@post.group.id, @post)
+  end
+
+  def locale(action)
+    I18n.t('notice.success', action:, resource: 'Comment')
   end
 end
