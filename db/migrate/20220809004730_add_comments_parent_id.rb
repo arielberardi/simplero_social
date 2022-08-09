ccf4c8d8fc13d1c679aa01033c7c358590ade439
arielberardi <ariel.berardi95@gmail.com>
2022-08-08 23:17:40 -0300

    Add replies relation to comments

---
+++ db/migrate/20220809004730_add_comments_parent_id.rb
@@ -0,0 +1,5 @@
+class AddCommentsParentId < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :comments, :parent, index: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_08_013941) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_004730) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,6 +56,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_08_013941) do
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "parent_id"
+    t.index ["parent_id"], name: "index_comments_on_parent_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
