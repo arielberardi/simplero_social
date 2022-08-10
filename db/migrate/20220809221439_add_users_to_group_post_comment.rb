53052f7ed928cfb8adfac4bde2217ff5a3c67456
arielberardi <ariel.berardi95@gmail.com>
2022-08-09 23:34:34 -0300

    Fix validation from rubocop

--- db/migrate/20220809221439_add_users_to_group_post_comment.rb
+++ db/migrate/20220809221439_add_users_to_group_post_comment.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddUsersToGroupPostComment < ActiveRecord::Migration[7.0]
   def change
     add_reference :groups, :user, index: true, null: false, foreign_key: true
