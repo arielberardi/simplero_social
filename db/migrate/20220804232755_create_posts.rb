e0f04c84d8bd2ef85b4f9ccc29bd56d934ba767b
arielberardi <ariel.berardi95@gmail.com>
2022-08-09 23:28:00 -0300

    Fix rubocop NotNull validations

--- db/migrate/20220804232755_create_posts.rb
+++ db/migrate/20220804232755_create_posts.rb
@@ -3,7 +3,7 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.string :title, null: false, default: ''
+      t.string :title, null: false
 
       t.timestamps
     end
--- db/migrate/20220805204115_add_posts_to_groups.rb
+++ db/migrate/20220805204115_add_posts_to_groups.rb
@@ -2,7 +2,7 @@
 
 class AddPostsToGroups < ActiveRecord::Migration[7.0]
   def change
-    add_reference :posts, :group, null: false, foreign_key: true, default: 0
+    add_reference :posts, :group, null: false, foreign_key: true
     add_index :posts, %i[title group_id], unique: true
   end
 end
